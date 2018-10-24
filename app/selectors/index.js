bf192f3cbfeff78124b28d72f9b8cc59220088e3
Edivan Silva <codedavinci.now@gmail.com>
2018-10-24 13:56:34 -0400

    feat(Selector that fetches the album genre by the artistId):

--- app/selectors/index.js
+++ app/selectors/index.js
@@ -1,1 +1,11 @@
+import { safeGet } from '../utils'
+
 export const getArtistByIdSelector = (artists, id) => artists.find(a => a.id === id)
+
+export const getGenreByArtistIdSelector = (artists, id) => {
+  const artist = getArtistByIdSelector(artists, id)
+
+  const genre = safeGet(['genres', 0], artist) || 'N/A'
+
+  return genre
+}
