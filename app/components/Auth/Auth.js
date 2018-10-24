4fea0dd4ec14a18867efbf00f17876f7789e7e23
Edivan Silva <codedavinci.now@gmail.com>
2018-10-24 08:47:48 -0400

    feat(Auth): Placeholder for Auth mechanism - WIP

---
+++ app/components/Auth/Auth.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { AUTHORIZE_URL } from '../../constants'
+
+const Auth = () => (
+  <div>
+    <a href={AUTHORIZE_URL}>click here</a>
+  </div>
+)
+
+export default Auth
