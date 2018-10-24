31d01f7c0817ede8df000a4585ed1a4ed2f20e43
Edivan Silva <codedavinci.now@gmail.com>
2018-10-24 08:35:52 -0400

    feat(Search): Add capabilities to search artists

--- app/components/Search/Search.js
+++ app/components/Search/Search.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react'
+
+import { Button } from '../Common'
 import './search.css'
 
 import PropTypes from 'prop-types'
 
-const Search = ({ onSubmitSearch }) => {
+const Search = ({ handleSearch }) => {
   let Input
 
   const onSubmit = e => {
     e.preventDefault()
     if (Input.value) {
-      onSubmitSearch(Input.value)
+      handleSearch(Input.value)
     }
   }
 
@@ -17,7 +19,7 @@ const Search = ({ onSubmitSearch }) => {
     <div className="search-wrapper">
       <form onSubmit={onSubmit}>
         <input type="text" ref={e => (Input = e)} />
-        <button type="submit">SEARCH</button>
+        <Button type="submit">SEARCH</Button>
       </form>
     </div>
   )
