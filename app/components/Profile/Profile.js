b9181972dc85cd180e950fedf3710a909266d7ca
Edivan Silva <codedavinci.now@gmail.com>
2018-10-24 08:50:38 -0400

    feat(Profile): Add Profile Component

---
+++ app/components/Profile/Profile.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+
+import AlbumList from '../AlbumList'
+import { ProfileBanner } from '../Common'
+
+import './profile.css'
+
+class Profile extends Component {
+  componentDidMount() {
+    const { params, handleAlbums } = this.props
+    handleAlbums(params.artistId)
+  }
+
+  render() {
+    const { params } = this.props
+    return (
+      <div className="profile-wrapper">
+        <ProfileBanner artistId={params.artistId} />
+        <AlbumList />
+      </div>
+    )
+  }
+}
+
+export default Profile
