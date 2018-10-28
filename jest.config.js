84afea43569776d8a23be0392fe2ab74a18de0b3
Edivan Silva <codedavinci.now@gmail.com>
2018-10-28 16:16:41 -0400

    test(Jest): Update config to leverage jest-enzyme methods

--- jest.config.js
+++ jest.config.js
@@ -4,11 +4,11 @@ module.exports = {
   setupFiles: ['<rootDir>/config/jest/setup.js'],
   moduleNameMapper: {
     '\\.css$': 'identity-obj-proxy',
-    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+    '^.+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/config/jest/__mocks__/fileMock.js',
-    transform: {
-      '^.+\\.jsx$': 'babel-jest',
-      '^.+\\.js$': 'babel-jest',
-    },
   },
+  setupTestFrameworkScriptFile: path.resolve(
+    'node_modules/jest-enzyme/lib/index.js'
+  ),
+  testEnvironment: 'enzyme',
 }
