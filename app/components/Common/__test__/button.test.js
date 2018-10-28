f2b045afd9257576261e0c8ae80311de31be6881
Edivan Silva <codedavinci.now@gmail.com>
2018-10-28 16:17:48 -0400

    test(Button): Implementation of 100% test coverage on Button

--- app/components/Common/__test__/button.test.js
+++ app/components/Common/__test__/button.test.js
@@ -3,19 +3,35 @@ import React from 'react'
 import { Button } from '../'
 
 describe('<Button />', () => {
+  global.console = { error: jest.fn() }
+
   const doShallow = (overrides = {}) => {
     const button = shallow(<Button {...overrides}>Submit</Button>)
-
     return button
   }
 
   it('renders', () => {
-    const button = render(<Button>Submit</Button>)
+    const button = doShallow()
     expect(button).toMatchSnapshot()
   })
 
   it('should have the size small as default', () => {
     let button = doShallow()
-    expect(button).toHaveProp('size', 'small')
+    expect(button).toHaveClassName('custom-button', 'small')
+  })
+
+  it('should not be allowed to render without children', () => {
+    shallow(<Button />)
+    expect(console.error).toHaveBeenCalled()
+  })
+
+  it('should be able to inject new props or attributes', () => {
+    let button = doShallow({ name: 'btn' })
+    expect(button).toHaveProp('name', 'btn')
+  })
+
+  it('should be able to disable', () => {
+    let button = doShallow({ disabled: true })
+    expect(button).toHaveProp('disabled')
   })
 })
