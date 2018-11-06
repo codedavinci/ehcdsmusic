7e1349e35c3ed539cd83f90b10045fec3784dfe2
Edivan Silva <codedavinci.now@gmail.com>
2018-11-06 04:41:51 -0500

    ci(Deploy): Deploy V2 with Serve

--- server.js
+++ server.js
@@ -1,19 +1,15 @@
-const express = require('express');
+const express = require('express')
 // const favicon = require('express-favicon');
-const path = require('path');
-const port = process.env.PORT || 8080;
-const app = express();
+const path = require('path')
+const port = process.env.PORT || 3000
+const app = express()
 
 // app.use(favicon(__dirname + '/build/favicon.ico'));
 
-app.use(express.static(__dirname));
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(__dirname))
+app.use(express.static(path.join(__dirname, 'dist')))
 
-app.get('/ping', function (req, res) {
- return res.send('pong');
-});
-
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
-});
-app.listen(port);
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'))
+})
+app.listen(port)
